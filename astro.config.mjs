2b7fabd0bf0157e0f88067b4dd73a632aa495a59
Elvis Dragon Mao <info@elvismao.com>
2026-05-14 23:55:20 +0800

    home: first draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		build: {
+			chunkSizeWarningLimit: 700,
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("/node_modules/") && id.includes("three")) {
+							return "vendor-three";
+						}
+
+						if (id.includes("/node_modules/") && (id.includes("gsap") || id.includes("lenis"))) {
+							return "vendor-animation";
+						}
+					}
+				}
+			}
+		}
+	}
+});
